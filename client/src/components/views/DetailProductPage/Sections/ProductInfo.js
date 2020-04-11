3de6f1d94faff2c8a2e4eeefcebd7d0dc879458e
John Ahn <smileajw1004@gmail.com>
2020-04-11 20:08:05 +0900

    Detail Product Page Completed

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Button, Descriptions } from 'antd';
-
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../../../_actions/user_actions';
 function ProductInfo(props) {
+    const dispatch = useDispatch();
+
 
     const clickHandler = () => {
+        //필요한 정보를 Cart 필드에다가 넣어 준다.
+        dispatch(addToCart(props.detail._id))
 
     }
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -68,4 +68,55 @@ router.get("/logout", auth, (req, res) => {
     });
 });
 
+
+router.post("/addToCart", auth, (req, res) => {
+
+    //먼저  User Collection에 해당 유저의 정보를 가져오기 
+    User.findOne({ _id: req.user._id },
+        (err, userInfo) => {
+
+            // 가져온 정보에서 카트에다 넣으려 하는 상품이 이미 들어 있는지 확인 
+
+            let duplicate = false;
+            userInfo.cart.forEach((item) => {
+                if (item.id === req.body.productId) {
+                    duplicate = true;
+                }
+            })
+
+            //상품이 이미 있을때
+            if (duplicate) {
+                User.findOneAndUpdate(
+                    { _id: req.user._id, "cart.id": req.body.productId },
+                    { $inc: { "cart.$.quantity": 1 } },
+                    { new: true },
+                    (err, userInfo) => {
+                        if (err) return res.status(200).json({ success: false, err })
+                        res.status(200).send(userInfo.cart)
+                    }
+                )
+            }
+            //상품이 이미 있지 않을때 
+            else {
+                User.findOneAndUpdate(
+                    { _id: req.user._id },
+                    {
+                        $push: {
+                            cart: {
+                                id: req.body.productId,
+                                quantity: 1,
+                                date: Date.now()
+                            }
+                        }
+                    },
+                    { new: true },
+                    (err, userInfo) => {
+                        if (err) return res.status(400).json({ success: false, err })
+                        res.status(200).send(userInfo.cart)
+                    }
+                )
+            }
+        })
+});
+
 module.exports = router;

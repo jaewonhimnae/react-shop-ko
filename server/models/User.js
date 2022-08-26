8d9c0cc35a024651dc0b14ec7dd99d1bedd448ce
John Ahn <smileajw1004@gmail.com>
2022-08-26 16:32:45 +0900

    bcryptjs changed

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 const moment = require("moment");
